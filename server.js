da18b3f77825318e9ec04ae469ca59542d4351a6
TAAMAOL3 <oliver.maegert@swisscom.com>
2024-08-13 07:35:54 +0200

    v3

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const mysql = require('mysql');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Setup database connection
+const db = mysql.createConnection({
+  host: 'reactappbackend.mysql.database.azure.com',
+  user: 'ijzrmfgqx',
+  password: 'IxL6keSl4NehCb0Do2Yt',
+  database: 'consultprofeedbackdb',
+  port: 3306,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, 'config', 'DigiCertGlobalRootG2.crt.pem'))
+  }
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('Error connecting to the Azure MySQL database:', err);
+    return;
+  }
+  console.log('Connected to Azure MySQL database!');
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
+
 // Store the current random number
 let currentRandomNumber = generateRandomNumber();
 
