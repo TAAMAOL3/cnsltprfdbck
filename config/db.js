892609ffcbc412a5fb9308ca01621e81856f5b21
TAAMAOL3 <oliver.maegert@swisscom.com>
2024-08-27 22:31:54 +0200

    newc

--- config/db.js
+++ config/db.js
@@ -1,13 +1,22 @@
 const mysql = require('mysql');
+const fs = require('fs');
 
-// Erstellen der Datenbankverbindung
-const db = mysql.createConnection({
+// Erstellen der Konfiguration für die Datenbankverbindung
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306
-});
+  port: 3306,
+};
+
+// Nur in der Produktionsumgebung wird die SSL-Option hinzugefügt
+if (process.env.NODE_ENV === 'production') {
+  dbConfig.ssl = { ca: fs.readFileSync(__dirname + '/DigiCertGlobalRootG2.crt.pem') };
+}
+
+// Erstellen der Datenbankverbindung
+const db = mysql.createConnection(dbConfig);
 
 // Verbindungsaufbau
 db.connect(err => {
