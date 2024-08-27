13c9494ec1928602c437b9dbf9214f74ffc2dcf8
TAAMAOL3 <oliver.maegert@swisscom.com>
2024-08-27 23:07:00 +0200

    dfafssad

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: 3306,
+  port: 3306
 };
 
 // Nur in der Produktionsumgebung wird die SSL-Option hinzugefügt
